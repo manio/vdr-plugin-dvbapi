f3ad2d59765d5103b797bf8889f5082840767eab
Mariusz Bialonczyk <manio@skyboo.net>
2012-03-15 16:09:01 +0100

    Fix last commit to work with earlier vdr versions
    Previous fix in commit ebdfc05 is using Adapter() function which was
    introduced in vdr 1.7.23; add conditional fallback code for earlier
    vdr versions. Thanks to @p100 for the hint.

--- SCCAMSlot.cpp
+++ SCCAMSlot.cpp
@@ -286,7 +286,11 @@ void SCCAMSlot::Process(const unsigned char *data, int len)
           }
           SCDVBDevice *dev = dynamic_cast<SCDVBDevice *>(Device());
           if (dev)
+#if VDRVERSNUM >= 10723
             dev->GetSCCIAdapter()->ProcessSIDRequest(dev->Adapter(), sid, ca_lm, vdr_caPMT, vdr_caPMTLen);
+#else
+            dev->GetSCCIAdapter()->ProcessSIDRequest(Device()->DeviceNumber(), sid, ca_lm, vdr_caPMT, vdr_caPMTLen);
+#endif
           else
             ERRORLOG("%d.%d cannot find CIAdapter for ProcessSIDRequest", cardIndex, slot);
         }
