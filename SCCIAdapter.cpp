manio/vdr-plugin-dvbapi

commit 2e6c1bac1ee3fc92624b131bbfacb872fe049ef3

Fix sending keys via ioctl to ca device

SCCIAdapter.cpp
@@ -38,7 +38,7 @@
 #define T_RCV          0x81
 #define T_DATA_LAST    0xA0
 
-SCCIAdapter::SCCIAdapter(cDevice *Device, int cardIndex)
+SCCIAdapter::SCCIAdapter(cDevice *Device, int cardIndex, int cafd)
 {
   for (int i = 0; i < MAX_SOCKETS; i++)
   {
@@ -49,6 +49,7 @@ SCCIAdapter::SCCIAdapter(cDevice *Device, int cardIndex)
   this->cardIndex = cardIndex;
   device = Device;
   capmt = new CAPMT;
+  fd_ca = cafd;
   decsa = new DeCSA(cardIndex);
   UDPSocket::bindx(this);
 
@@ -361,6 +362,11 @@ void SCCIAdapter::ProcessSIDRequest(int card_index, int sid, int ca_lm, const un
 bool SCCIAdapter::DeCSASetCaDescr(ca_descr_t *ca_descr)
 {
   DEBUGLOG("%s: index=%d", __FUNCTION__, ca_descr->index);
+  if (fd_ca >= 0)
+  {
+    cMutexLock lock(&cafdMutex);
+    return ioctl(fd_ca, CA_SET_DESCR, ca_descr) >= 0;
+  }
   if (ca_descr->index == (unsigned) -1)
   {
     DEBUGLOG("%s: removal request - ignoring", __FUNCTION__);
@@ -374,6 +380,11 @@ bool SCCIAdapter::DeCSASetCaDescr(ca_descr_t *ca_descr)
 bool SCCIAdapter::DeCSASetCaPid(ca_pid_t *ca_pid)
 {
   DEBUGLOG("%s: PID=%d, index=%d", __FUNCTION__, ca_pid->pid, ca_pid->index);
+  if (fd_ca >= 0)
+  {
+    cMutexLock lock(&cafdMutex);
+    return ioctl(fd_ca, CA_SET_PID, ca_pid) >= 0;
+  }
   if (ca_pid->index == -1)
   {
     DEBUGLOG("%s: removal request - ignoring", __FUNCTION__);

SCCIAdapter.h
@@ -52,8 +52,10 @@ private:
   int caidsLength;
   int cardIndex;
   cMutex ciMutex;
+  cMutex cafdMutex;
   SCCAMSlot *slots[MAX_CI_SLOTS];
   int version[MAX_CI_SLOTS];
+  int fd_ca;
   cTimeMs caidTimer, triggerTimer;
   int tcid;
   cTimeMs readTimer, writeTimer;
@@ -64,7 +66,7 @@ private:
   int addCaid(int offset, int limit, unsigned short caid);
 
 public:
-  SCCIAdapter(cDevice *Device, int CardIndex);
+  SCCIAdapter(cDevice *Device, int CardIndex, int cafd);
   ~SCCIAdapter();
   int Adapter()
   {

device-tmpl.cpp
@@ -30,7 +30,6 @@ private:
   cTimeMs lastDump;
   SCCIAdapter *sCCIAdapter;
   int fd_dvr, fd_ca, fd_ca2;
-  cMutex cafdMutex;
   bool softcsa;
   char devId[8];
   bool ScActive(void);
@@ -66,7 +65,7 @@ SCDEVICE::SCDEVICE(cScDevicePlugin *DevPlugin, int Adapter, int Frontend, int ca
   fd_ca2 = dup(fd_ca);
   fd_dvr = -1;
   snprintf(devId, sizeof(devId), "%d/%d", Adapter, Frontend);
-  sCCIAdapter = new SCCIAdapter(this, Adapter);
+  sCCIAdapter = new SCCIAdapter(this, Adapter, cafd);
   DEBUGLOG("%s: done", __FUNCTION__);
 }
 

commit 2df8c881db44c9541b8990ede0b1820f595fdaa1

Call ioctl on UFS9xx device output plugin only
Add conditional flags and do ioctl on ca device currently only for
UFS9xx device plugin, because commit 2e6c1ba broke recordings.

Fixes #5

SCCIAdapter.cpp
@@ -362,11 +362,13 @@ void SCCIAdapter::ProcessSIDRequest(int card_index, int sid, int ca_lm, const un
 bool SCCIAdapter::DeCSASetCaDescr(ca_descr_t *ca_descr)
 {
   DEBUGLOG("%s: index=%d", __FUNCTION__, ca_descr->index);
+#ifdef WITH_UFS9XX
   if (fd_ca >= 0)
   {
     cMutexLock lock(&cafdMutex);
     return ioctl(fd_ca, CA_SET_DESCR, ca_descr) >= 0;
   }
+#endif
   if (ca_descr->index == (unsigned) -1)
   {
     DEBUGLOG("%s: removal request - ignoring", __FUNCTION__);
@@ -380,11 +382,13 @@ bool SCCIAdapter::DeCSASetCaDescr(ca_descr_t *ca_descr)
 bool SCCIAdapter::DeCSASetCaPid(ca_pid_t *ca_pid)
 {
   DEBUGLOG("%s: PID=%d, index=%d", __FUNCTION__, ca_pid->pid, ca_pid->index);
+#ifdef WITH_UFS9XX
   if (fd_ca >= 0)
   {
     cMutexLock lock(&cafdMutex);
     return ioctl(fd_ca, CA_SET_PID, ca_pid) >= 0;
   }
+#endif
   if (ca_pid->index == -1)
   {
     DEBUGLOG("%s: removal request - ignoring", __FUNCTION__);