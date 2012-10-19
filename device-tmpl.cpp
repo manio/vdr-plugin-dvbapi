manio/vdr-plugin-dvbapi

commit 7c44be5bed57f4d7769db203940b853739530090

another fullts/softcsa mode fixes

device-tmpl.cpp
@@ -66,7 +66,6 @@ SCDEVICE::SCDEVICE(cScDevicePlugin *DevPlugin, int Adapter, int Frontend, int ca
   fd_ca2 = dup(fd_ca);
   fd_dvr = -1;
   snprintf(devId, sizeof(devId), "%d/%d", Adapter, Frontend);
-  sCCIAdapter = new SCCIAdapter(this, Adapter, cafd);
   DEBUGLOG("%s: done", __FUNCTION__);
 }
 
@@ -123,6 +122,7 @@ void SCDEVICE::LateInit(void)
     else
       INFOLOG("Using software decryption on card %s", devId);
   }
+  sCCIAdapter = new SCCIAdapter(this, n, fd_ca, softcsa, fullts);
 }
 
 bool SCDEVICE::HasCi(void)

SCCIAdapter.cpp
@@ -38,7 +38,7 @@
 #define T_RCV          0x81
 #define T_DATA_LAST    0xA0
 
-SCCIAdapter::SCCIAdapter(cDevice *Device, int cardIndex, int cafd)
+SCCIAdapter::SCCIAdapter(cDevice *Device, int cardIndex, int cafd, bool SoftCSA, bool FullTS)
 {
   for (int i = 0; i < MAX_SOCKETS; i++)
   {
@@ -50,6 +50,9 @@ SCCIAdapter::SCCIAdapter(cDevice *Device, int cardIndex, int cafd)
   device = Device;
   capmt = new CAPMT;
   fd_ca = cafd;
+  softcsa = SoftCSA;
+  fullts = FullTS;
+
   decsa = new DeCSA(cardIndex);
   UDPSocket::bindx(this);
 
@@ -362,13 +365,11 @@ void SCCIAdapter::ProcessSIDRequest(int card_index, int sid, int ca_lm, const un
 bool SCCIAdapter::DeCSASetCaDescr(ca_descr_t *ca_descr)
 {
   DEBUGLOG("%s: index=%d", __FUNCTION__, ca_descr->index);
-#ifdef WITH_UFS9XX
-  if (fd_ca >= 0)
+  if (!softcsa || (fullts && ca_descr->index == 0))
   {
     cMutexLock lock(&cafdMutex);
     return ioctl(fd_ca, CA_SET_DESCR, ca_descr) >= 0;
   }
-#endif
   if (ca_descr->index == (unsigned) -1)
   {
     DEBUGLOG("%s: removal request - ignoring", __FUNCTION__);
@@ -382,13 +383,11 @@ bool SCCIAdapter::DeCSASetCaDescr(ca_descr_t *ca_descr)
 bool SCCIAdapter::DeCSASetCaPid(ca_pid_t *ca_pid)
 {
   DEBUGLOG("%s: PID=%d, index=%d", __FUNCTION__, ca_pid->pid, ca_pid->index);
-#ifdef WITH_UFS9XX
-  if (fd_ca >= 0)
+  if (!softcsa || (fullts && ca_pid->index == 0))
   {
     cMutexLock lock(&cafdMutex);
     return ioctl(fd_ca, CA_SET_PID, ca_pid) >= 0;
   }
-#endif
   if (ca_pid->index == -1)
   {
     DEBUGLOG("%s: removal request - ignoring", __FUNCTION__);

SCCIAdapter.h
@@ -47,6 +47,7 @@ private:
   DeCSA *decsa;
   CAPMT *capmt;
   cDevice *device;
+  bool softcsa, fullts;
   bool initialCaDscr;
   unsigned short caids[1024];
   int caidsLength;
@@ -66,7 +67,7 @@ private:
   int addCaid(int offset, int limit, unsigned short caid);
 
 public:
-  SCCIAdapter(cDevice *Device, int CardIndex, int cafd);
+  SCCIAdapter(cDevice *Device, int CardIndex, int cafd, bool SoftCSA, bool FullTS);
   ~SCCIAdapter();
   int Adapter()
   {

commit a98ff1e801aa142ffeae4058e90aae5e3be40f02

Fix a bug introduced in 7c44be5
On some slower machines (eg. ION) creating SCCIAdapter later in LateInit()
prevents oscam to working correctly when PMT data arrive, leading to opening
demux problem and following error message in log:
c dvbapi: ERROR: Read error on fd 14 (errno=110 Connection timed out)

Thanks to dreamvor for reporting the problem and for test environment.

device-tmpl.cpp
@@ -63,11 +63,15 @@ SCDEVICE::SCDEVICE(cScDevicePlugin *DevPlugin, int Adapter, int Frontend, int ca
   tsBuffer = 0;
   hwciadapter = 0;
   devplugin = DevPlugin;
-  softcsa = fullts = false;
+  fullts = false;
+  softcsa = (cafd < 0);
+  if (softcsa)
+    fullts = CheckFullTs();
   fd_ca = cafd;
   fd_ca2 = dup(fd_ca);
   fd_dvr = -1;
   snprintf(devId, sizeof(devId), "%d/%d", Adapter, Frontend);
+  sCCIAdapter = new SCCIAdapter(this, Adapter, cafd, softcsa, fullts);
   DEBUGLOG("%s: done", __FUNCTION__);
 }
 
@@ -107,7 +111,6 @@ void SCDEVICE::LateInit(void)
   int n = CardIndex();
   if (DeviceNumber() != n)
     ERRORLOG("CardIndex - DeviceNumber mismatch! Put DVBAPI plugin first on VDR commandline!");
-  softcsa = (fd_ca < 0);
   if (softcsa)
   {
     if (HasDecoder())
@@ -120,7 +123,6 @@ void SCDEVICE::LateInit(void)
   }
   if (softcsa)
   {
-    fullts = CheckFullTs();
     if (fullts)
       INFOLOG("Enabling hybrid full-ts mode on card %s", devId);
     else
@@ -128,7 +130,6 @@ void SCDEVICE::LateInit(void)
   }
   if (fd_ca2 >= 0)
     hwciadapter = cDvbCiAdapter::CreateCiAdapter(this, fd_ca2);
-  sCCIAdapter = new SCCIAdapter(this, n, fd_ca, softcsa, fullts);
 }
 
 bool SCDEVICE::HasCi(void)