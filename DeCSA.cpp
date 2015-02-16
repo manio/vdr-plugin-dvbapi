manio/vdr-plugin-dvbapi

commit 36a6b1d2642bcce66300e1e4574062af6f70d326

DeCSA: use std::map and don't rely on max adapter constant
Finally get rid of MAX_ADAPTERS allowing any adapter index from OSCam.
Should also reduce memory usage of the plugin.

DeCSA.cpp
@@ -50,7 +50,6 @@ DeCSA::DeCSA(int CardIndex)
   memset(cs_key_odd, 0, sizeof(cs_key_odd));
 #endif
   memset(cwSeen, 0, sizeof(cwSeen));
-  memset(pidmap, 0, sizeof(pidmap));
   ResetState();
 }
 
@@ -128,10 +127,9 @@ bool DeCSA::SetDescr(ca_descr_t *ca_descr, bool initial)
 bool DeCSA::SetCaPid(uint8_t adapter_index, ca_pid_t *ca_pid)
 {
   cMutexLock lock(&mutex);
-  if (ca_pid->index < MAX_CSA_IDX && ca_pid->pid < MAX_CSA_PIDS &&
-      adapter_index >= 0 && adapter_index < MAX_ADAPTERS)
+  if (ca_pid->index < MAX_CSA_IDX && ca_pid->pid < MAX_CSA_PIDS)
   {
-    pidmap[adapter_index][ca_pid->pid] = ca_pid->index;
+    pidmap[make_pair(adapter_index, ca_pid->pid)] = ca_pid->index;
     DEBUGLOG("%d.%d: set pid 0x%04x", cardindex, ca_pid->index, ca_pid->pid);
   }
   else
@@ -207,7 +205,7 @@ bool DeCSA::Decrypt(uint8_t adapter_index, unsigned char *data, int len, bool fo
       offset = ts_packet_get_payload_offset(data + l);
       payload_len = TS_SIZE - offset;
 #endif
-      int idx = pidmap[adapter_index][((data[l + 1] << 8) + data[l + 2]) & (MAX_CSA_PIDS - 1)];
+      int idx = pidmap[make_pair(adapter_index, ((data[l + 1] << 8) + data[l + 2]) & (MAX_CSA_PIDS - 1))];
       if (currIdx < 0 || idx == currIdx)
       {                         // same or no index
         currIdx = idx;

DeCSA.h
@@ -19,6 +19,7 @@
 #ifndef ___DECSA_H
 #define ___DECSA_H
 
+#include <map>
 #include <linux/dvb/ca.h>
 #include <vdr/dvbdevice.h>
 #include <vdr/thread.h>
@@ -31,10 +32,11 @@ extern "C" {
 
 #define MAX_CSA_PIDS 8192
 #define MAX_CSA_IDX  16
-#define MAX_ADAPTERS 4
 #define MAX_KEY_WAIT 20         // max seconds to consider a CW as valid
 
 #include "DVBAPI.h"
+
+using namespace std;
 
 class DeCSA
 {
@@ -50,7 +52,7 @@ private:
   struct dvbcsa_bs_key_s *cs_key_odd[MAX_CSA_IDX];
 #endif
   time_t cwSeen[MAX_CSA_IDX];   // last time the CW for the related key was seen
-  unsigned char pidmap[MAX_ADAPTERS][MAX_CSA_PIDS];    //FIXME: change to dynamic structure
+  map<pair<int, int>, unsigned char> pidmap;
   cMutex mutex;
   int cardindex;
   bool GetKeyStruct(int idx);

commit 99852d0568fb9ffe2c9804dfe68b548d4f6d9eb9

Fix segmentation faults introduced in 36a6b1d

DeCSA.cpp
@@ -129,7 +129,7 @@ bool DeCSA::SetCaPid(uint8_t adapter_index, ca_pid_t *ca_pid)
   cMutexLock lock(&mutex);
   if (ca_pid->index < MAX_CSA_IDX && ca_pid->pid < MAX_CSA_PIDS)
   {
-    pidmap[make_pair(adapter_index, ca_pid->pid)] = ca_pid->index;
+    pidmap[make_pair(adapter_index, ca_pid->pid)] = ca_pid->index == -1 ? 0 : ca_pid->index;
     DEBUGLOG("%d.%d: set pid 0x%04x", cardindex, ca_pid->index, ca_pid->pid);
   }
   else