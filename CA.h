7d51cc457823156d446d06c6ae40f850958fa735
Mariusz Bialonczyk <manio@skyboo.net>
2017-11-25 18:57:08 +0100

    Fix compilation for kernels >= 4.14
    Closes #122

--- CA.h
+++ CA.h
@@ -9,4 +9,12 @@ typedef struct ca_descr_aes {
   unsigned char cw[16];
 } ca_descr_aes_t;
 
+#ifndef CA_SET_PID /* removed in kernel 4.14 */
+typedef struct ca_pid {
+        unsigned int pid;
+        int index;          /* -1 == disable */
+} ca_pid_t;
+#define CA_SET_PID _IOW('o', 135, struct ca_pid)
 #endif
+
+#endif // __CA_H
