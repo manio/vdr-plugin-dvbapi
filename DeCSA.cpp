3ca4dc865d6c0460dc947b8e1472de32c5ebd193
root <ua0lnj@bk.ru>
2022-12-09 15:31:41 +1000

    Fix for patched libdvbcsa.

--- DeCSA.cpp
+++ DeCSA.cpp
@@ -419,9 +419,9 @@ bool DeCSAKey::Set_even_control_word(const unsigned char *even, const unsigned c
   if (cs_key_even)
   {
 #ifdef LIBDVBCSA_NEW
-    dvbcsa_bs_key_set(even, cs_key_even,ecm); //todo lib must be upgraded to support this.
+    dvbcsa_bs_key_set_ecm(even, cs_key_even, ecm); //todo lib must be upgraded to support this.
 #else
-    dvbcsa_bs_key_set(even, cs_key_even); //todo lib must be upgraded to support this.
+    dvbcsa_bs_key_set(even, cs_key_even);
 #endif
     return true;
   }
@@ -442,9 +442,9 @@ bool DeCSAKey::Set_odd_control_word(const unsigned char *odd, const unsigned cha
   if (cs_key_odd)
   {
 #ifdef LIBDVBCSA_NEW
-    dvbcsa_bs_key_set(odd, cs_key_odd, ecm); //todo lib must be upgraded to support this.
+    dvbcsa_bs_key_set_ecm(odd, cs_key_odd, ecm); //todo lib must be upgraded to support this.
 #else
-    dvbcsa_bs_key_set(odd, cs_key_odd); //todo lib must be upgraded to support this.
+    dvbcsa_bs_key_set(odd, cs_key_odd);
 #endif
     return true;
   }
